951a854c1f4ca02fb86140590f410a3823a4768b
Erica Gurung <egurung@student.unimelb.edu.au>
2025-01-17 15:18:39 +1100

    next js installation

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Erica Gurung | Portfolio',
+  description: 'todo: description',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-pink-50 text-gray-950 relative`}>
+        <div className="bg-fuchsia-200 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-red-200 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] -z-10 rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        
+        {children}
+        </body>
+    </html>
+  )
+}
