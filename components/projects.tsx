d59f98552465d88f9f4ed8437ecf8e93d558e2c0
Erica Gurung <egurung@student.unimelb.edu.au>
2025-02-14 12:06:15 +1100

    finish header

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,23 +1,13 @@
 "use client";
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionHeading from './sectionHeading'
 import { projectsData } from '@/lib/data'
-import { useInView } from 'react-intersection-observer';
 import Project from './project'
-import { useActiveSectionContext } from '@/context/activeSectionContext';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function Projects() {
-  const { ref, inView } = useInView({
-      threshold: 0.5,
-  });
-  const { setActiveSection } = useActiveSectionContext();
-
-  useEffect(() => {
-    if (inView) {
-      setActiveSection("Projects");
-    }
-  }, [inView, setActiveSection])
+  const { ref } = useSectionInView("Projects", 0.5);
 
   return (
     <section ref={ref} id="projects" className='scroll-mt-28'>
--- context/activeSectionContext.tsx
+++ context/activeSectionContext.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import React, { useState, createContext, useCallback, useContext } from 'react'
-import { links } from '@/lib/data';
-
-type SectionName = typeof links[number]["name"];
+import React, { useState, createContext, useContext } from 'react'
+import type { SectionName } from '@/lib/types';
 
 type ActiveSectionContextProviderProps = {
   children: React.ReactNode;
@@ -11,17 +9,22 @@ type ActiveSectionContextProviderProps = {
 
 type ActiveSectionContextType = {
   activeSection: SectionName;
-  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>
+  setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null);
 
 export default function ActiveSectionContextProvider({ children }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>('Home');
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0);
   return (
     <ActiveSectionContext.Provider value={{
       activeSection,
-      setActiveSection
+      setActiveSection,
+      timeOfLastClick,
+      setTimeOfLastClick,
     }}>
       { children }
     </ActiveSectionContext.Provider>)
